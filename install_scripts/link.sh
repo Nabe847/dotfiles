2a74c826c70a22598bd0cf6f336949aa88bd602e
Nabe847 <Nabe847@gmail.com>
2023-11-05 00:25:08 +0900

    fix: backup dir

--- install_scripts/link.sh
+++ install_scripts/link.sh
@@ -2,8 +2,8 @@ set +e
 rm -rf ./backup/link
 mkdir -p ./backup/link
 
-mv ~/.zprofile ./backup/ 2>/dev/null
+mv ~/.zprofile ./backup/link 2>/dev/null
 ln -s ~/dotfiles/.zprofile ~/
 
-mv ~/.zshrc ./backup/ 2>/dev/null
+mv ~/.zshrc ./backup/link 2>/dev/null
 ln -s ~/dotfiles/.zshrc ~/
