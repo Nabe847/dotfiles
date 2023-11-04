4384c9ead5e69772a7b34bb3df46337f2a825c59
Nabe847 <Nabe847@gmail.com>
2023-11-04 12:43:44 +0900

    add: setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Installing Xcode"
+xcode-select --install
+
+echo "Installing Homebrew"
+which /opt/homebrew/bin/brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew doctor
+which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew update --verbose
+which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew upgrade --verbose
+
+echo "Installing .Brewfile applications"
+which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew bundle --file ./.Brewfile --verbose
+
+which brew >/dev/null 2>&1 && brew cleanup --verbose
+
+echo "Installing programming laguages"
+./_asdf.sh
+
+echo "Creating symbolic links"
+./_link.sh
+
+exec $SHELL -l
