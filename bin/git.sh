aaa6106a01140f9757315ce186b8af30e77b8dfb
Nabe847 <Nabe847@gmail.com>
2023-11-05 21:57:14 +0900

    update: git alias

--- bin/git.sh
+++ bin/git.sh
@@ -1,6 +1,7 @@
 git config --global alias.co checkout
 git config --global alias.sw switch
 git config --global alias.br branch
-git config --global alias.ci commit
+git config --global alias.cm commit
 git config --global alias.st status
+git config --global alias.ps push
 git config --global core.editor "code --wait"
