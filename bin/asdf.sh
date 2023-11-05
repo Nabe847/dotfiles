7ffdd229f5fc9d02b93c5a94636600c30a785da1
Nabe847 <Nabe847@gmail.com>
2023-11-05 23:04:22 +0900

    fix: fix to reload configuration before pip --upgrade

--- bin/asdf.sh
+++ bin/asdf.sh
@@ -3,4 +3,6 @@ ln -s  ~/dotfiles/.config/asdf/.default-python-packages ~/
 asdf plugin-add python
 asdf install python latest
 asdf global python latest
+
+source "$HOME/.zshrc"
 pip install --upgrade pip
